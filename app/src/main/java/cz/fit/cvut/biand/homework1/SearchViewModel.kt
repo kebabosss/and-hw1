2cd360702eede6de1c847053b9c38d017389b4c9
Krbek Petr <krbekpet@fit.cvut.cz>
2025-03-31 22:59:43 +0200

    Search screen love

--- app/src/main/java/cz/fit/cvut/biand/homework1/SearchViewModel.kt
+++ app/src/main/java/cz/fit/cvut/biand/homework1/SearchViewModel.kt
@@ -1,27 +1,49 @@
 package cz.fit.cvut.biand.homework1
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import cz.fit.cvut.biand.homework1.model.Character
 import cz.fit.cvut.biand.homework1.model.characters
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.toList
 
 class SearchViewModel : ViewModel() {
     var searchQuery by mutableStateOf("")
+        private set
+
+
     private val characterFlow = flowOf(characters)
-    val searchResults = characterFlow
-        .map { list -> list.filter { it.name.contains(searchQuery, ignoreCase = true) } }
-        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+    val searchResults = snapshotFlow { searchQuery }
+        .combine(characterFlow) { searchQuery, character ->
+            when {
+                searchQuery.isNotEmpty() -> character.filter { character ->
+                    character.name.contains(searchQuery, ignoreCase = true)
+                }
+                else -> emptyList()
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            initialValue = emptyList(),
+            started = SharingStarted.WhileSubscribed(5_000)
+        )
 
     fun onSearchQueryChange(newQuery : String)
     {
         searchQuery = newQuery
+        Log.v("SEARCH", "Query has changed to $searchQuery")
     }
 
     fun clearQuery()
--- app/src/main/java/cz/fit/cvut/biand/homework1/screen/Search.kt
+++ app/src/main/java/cz/fit/cvut/biand/homework1/screen/Search.kt
@@ -1,5 +1,16 @@
 package cz.fit.cvut.biand.homework1.screen
 
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,17 +20,29 @@ import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import cz.fit.cvut.biand.homework1.Detail
 import cz.fit.cvut.biand.homework1.R
 import cz.fit.cvut.biand.homework1.SearchViewModel
+import cz.fit.cvut.biand.homework1.model.Character
+import cz.fit.cvut.biand.homework1.model.characters
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(navController: NavController, viewModel: SearchViewModel) {
     val searchResults by viewModel.searchResults.collectAsStateWithLifecycle()
+
     SearchBar(
         inputField = {
             SearchBarDefaults.InputField(
@@ -45,5 +68,39 @@ fun SearchScreen(navController: NavController, viewModel: SearchViewModel) {
         expanded = true,
         onExpandedChange = {  },
     ) {
+        LazyColumn(modifier = Modifier
+            .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            items(searchResults) { character ->
+                SearchItem(character, navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchItem(character: Character, navController: NavController)
+{
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .clickable { navController.navigate(Detail(character.id)) },
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+        ){
+        AsyncImage(
+            model = character.imgUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier =  Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .size(64.dp)
+
+        )
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(character.name, fontWeight = FontWeight.Bold)
+            Text(character.status)
+        }
     }
 }
